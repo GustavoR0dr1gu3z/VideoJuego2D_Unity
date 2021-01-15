9e45f070626a3c6a43bb50702e423a7716997a74
Gustavo Rodríguez Calzada <gustavo.soader.cx@gmail.com>
2021-01-15 17:09:29 -0600

    agregando el efecto parallax

---
+++ Assets/Scripts/gameController.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class gameController : MonoBehaviour
+{
+    //rango
+    [Range(0f,0.20f)]
+
+    //Creando metodos para velocidad
+    public float parallaxSpeed = 0.00002f;
+    public RawImage background;
+    public RawImage platform;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float finalSpeed = parallaxSpeed = Time.deltaTime;
+        background.uvRect = new Rect(background.uvRect.x + finalSpeed, 0f, 1f, 1f);
+        platform.uvRect = new Rect(platform.uvRect.x + finalSpeed * 4, 0f, 1f, 1f);
+    }
+}
